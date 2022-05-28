0effcd499cb0ed4f63efafd17c80351e42b9aab8
Ashish A. Naicker <ashishnaicker@gmail.com>
2022-05-28 17:32:48 +1200

    Route to controllers with templates

---
+++ LandonApi/Controllers/RootController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                href = Url.Link(nameof(GetRoot), null),
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomsController.GetRooms), null)
+                }
+            };
+            return Ok(response);
+        }
+    }
+}   
